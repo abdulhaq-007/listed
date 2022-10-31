fa9c6b6be20541c7a0b37cd9960d266a12604200
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2022-10-31 12:47:57 -0800

    homework done successfully

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def destination(request):
+    return render(request, 'destination.html')
+
+
+
+
+
